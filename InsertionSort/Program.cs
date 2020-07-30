1c21007035f4c5fb5004bd2aa9de4412306e6b0a
getrajer533 <getrajer533@gmail.com>
2020-07-30 19:40:27 +0100

    Add project files.

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace InsertionSort
+{
+   class InsertionSort
+    {
+        public int[] Sort(int[] array)
+        {
+            int j;
+
+            for(int i = 1; i < array.Length; i++)
+            {
+                j = i;
+
+                while((j > 0) && (array[j] < array[j - 1]))
+                {
+                    int buffer = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = buffer;
+                    j--;
+                }
+            }
+
+            return array;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] variables = new int[] { 3, 4, 32, 2, 1, 43, 3, 3, 5, 6 };
+
+            InsertionSort s = new InsertionSort();
+
+            int[] sorted = s.Sort(variables);
+
+            for(int i = 0; i < sorted.Length; i++)
+            {
+                Console.Write($"|{sorted[i]}|");
+            }
+            
+        }
+    }
+}
---
+++ Tour Optymization/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Tour_Optymization
+{
+
+    class FindShortestTourMyTake
+    {
+        public void ShortestFinderByNeighbour(int[] array)
+        {
+            if(array.Length > 0)
+            {
+                //find the mid point
+
+                int firstPoint = array.Length / 2;
+                int rightPoint = 0;
+                int leftPoint = 0;
+
+                for(int i = 0; i < array.Length; i += 2)
+                {
+                    if(i == 0)
+                    {
+                        Console.Write($"{array[firstPoint]},");
+                        rightPoint = firstPoint + 1;
+                        leftPoint = firstPoint - 1;
+                    }
+                    else
+                    {
+                        Console.Write($"{array[rightPoint]},");
+                        Console.Write($"{array[leftPoint]},");
+                        rightPoint++;
+                        leftPoint--;
+                    }
+                }
+
+                Console.Write(array[firstPoint]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new int[] { -21, -5, -1, 0, 1, 3, 11};
+
+            FindShortestTourMyTake finder = new FindShortestTourMyTake();
+
+            finder.ShortestFinderByNeighbour(values);
+
+        }
+    }
+}
