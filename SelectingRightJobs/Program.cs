50837c7243cd95b26ae85683fbb538ac28bddbf5
getrajer533 <getrajer533@gmail.com>
2020-07-31 15:34:20 +0100

    greedy algorithm

--- SelectingRightJobs/Program.cs
+++ SelectingRightJobs/Program.cs
@@ -35,7 +35,7 @@ namespace SelectingRightJobs
             if(jobs != null)
             {
                 //Sort the jobs by begin time
-                jobs.OrderBy(o => o.BeginJob);
+                jobs = jobs.OrderBy(o => o.BeginJob).ToList();
 
                 List<Job> JobsDone = new List<Job>();
 
@@ -65,7 +65,49 @@ namespace SelectingRightJobs
                     Console.WriteLine($"{JobsDone[i].Name} | Begin {JobsDone[i].BeginJob} | End {JobsDone[i].EndJob}");
                 }
 
-                Console.WriteLine("You have done 4 jobs with this algorithm");
+                Console.WriteLine($"You have done {JobsDone.Count} jobs with this algorithm");
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+
+    class GreedyAlgorithm
+    {
+        public void Sort(List<Job> jobs)
+        {
+            if (jobs != null) 
+            {
+                //Sort Jobs by end time
+                jobs = jobs.OrderBy(o => o.EndJob).ToList();
+
+                List<Job> JobsDone = new List<Job>();
+
+                for (int i = 0; i < jobs.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        JobsDone.Add(jobs[i]);
+                    }
+                    else
+                    {
+                        if(jobs[i].BeginJob >= JobsDone[JobsDone.Count - 1].EndJob)
+                        {
+                            JobsDone.Add(jobs[i]);
+                        }
+                    }
+                }
+
+
+                Console.WriteLine("Solution with greedy algorithm");
+
+                for (int i = 0; i < JobsDone.Count; i++)
+                {
+                    Console.WriteLine($"{JobsDone[i].Name} | Begin {JobsDone[i].BeginJob} | End {JobsDone[i].EndJob}");
+                }
+
+                Console.WriteLine($"You have done {JobsDone.Count} jobs with this algorithm");
                 Console.WriteLine();
             }
         }
@@ -80,19 +122,21 @@ namespace SelectingRightJobs
             {
                 new Job(1, 3, "Jungle"),
                 new Job(1, 4, "Algorist"),
+                new Job(4, 7, "Problem"),
+                new Job(6, 9, "Termintated"),
+                new Job(5, 6, "Programing"),
                 new Job(2, 3, "Mathematics"),
                 new Job(3, 4, "State"),
+                new Job(7, 9, "Bets"),
                 new Job(4, 5, "Tree"),
-                new Job(4, 7, "Problem"),
-                new Job(5, 6, "Programing"),
-                new Job(6, 9, "Termintated"),
-                new Job(7, 9, "Bets")
             };
 
 
             FromFirstThenNext s1 = new FromFirstThenNext();
+            GreedyAlgorithm s2 = new GreedyAlgorithm();
 
             s1.Sort(Jobs);
+            s2.Sort(Jobs);
         }
     }
 }
